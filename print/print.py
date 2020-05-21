47cd4e2ee0040b883e7ff0595d6489ca0a316638
lucasfernande <lucas.fernandes6192@outlook.com>
2020-05-21 12:17:37 -0300

    comando print

---
+++ print/print.py
@@ -0,0 +1,12 @@
+
+# print(123)
+print('Py', 'Charm')  # será adicionado um espaço entre as palavras
+print('Py', 'Charm', sep='-') # o print irá usar o traço como separador das palavras
+
+print('Py', 'Charm', end='') # o end faz com que o print não quebre uma linha ao final das palavras
+print(' Python')
+
+# 507.085.988-97
+
+print('123' , '456', '789', sep='.', end='-') # mascara simples de cpf
+print(00)
