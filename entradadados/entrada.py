cce8d5bb9055291b631f1a51dc775af5b17d36e6
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-09 15:32:18 -0300

    entrada de dados

---
+++ entradadados/entrada.py
@@ -0,0 +1,20 @@
+"""
+nome = input("Qual o seu nome? ")
+idade = input("Qual a sua idade: ")
+
+anoNasc = 2020 - int (idade) # cast de str para int
+
+print()
+print(f'{nome} tem {idade} anos.')
+print(f'{nome} nasceu em {anoNasc}')
+
+"""
+
+print('------ Soma -------')
+print()
+num1 = input('Primeiro número: ')
+num2 = input('Segundo número : ')
+
+soma = int (num1) + int (num2)
+
+print('Resultado:', soma)
