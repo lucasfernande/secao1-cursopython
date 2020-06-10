b017866e41c003ef01b96a796c489d98a81b9e15
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-09 21:05:14 -0300

    operadores lógicos

---
+++ operadores-relacionais/oplogicos.py
@@ -0,0 +1,24 @@
+# operadores logicos
+
+# and, or, not, in e not in
+
+a = 2
+b = 2
+c = 4
+
+if a == b and b < c:
+    print('Verdadeiro')
+else:
+    print('Falso')
+
+nome = 'Lucas'
+
+if 'u' in nome:
+    print('Verdadeiro')
+else:
+    print('Falso')
+
+if 'u' not in nome:
+    print('Verdadeiro')
+else:
+    print('Falso')
