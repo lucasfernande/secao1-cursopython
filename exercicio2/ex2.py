82cb1bb11d2d9615c1a61b18404683bd8fc21bd1
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-15 14:15:40 -0300

    correção

--- exercicio2/ex2.py
+++ exercicio2/ex2.py
@@ -2,12 +2,14 @@ hora = input('Digite a hora atual: ')
 
 if hora.isdigit():
     hora = int(hora)
-
-    if hora <= 11:
-        print('Bom dia.')
-    elif hora <= 17:
-        print('Boa tarde.')
+    if hora < 0 or hora > 23:
+        print('O horário deve estar entre 0 e 23')
     else:
-        print('Boa noite.')
+        if hora <= 11:
+            print('Bom dia.')
+        elif hora <= 17:
+            print('Boa tarde.')
+        else:
+            print('Boa noite.')
 else:
     print('Erro: o valor digitado não era um número')
