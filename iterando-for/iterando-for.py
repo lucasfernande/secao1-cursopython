c38516f9ce6a4e8f21f7b07e7ad78d0dbe7a2bff
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-16 16:00:43 -0300

    modificando letras de uma palavra

--- iterando-for/iterando-for.py
+++ iterando-for/iterando-for.py
@@ -1,4 +1,15 @@
 texto = 'python'
+novo = ''
 
 for letra in texto:
     print(letra)
+
+for letra in texto:
+    if letra == 'p':
+        novo += letra.upper()
+    elif letra == 'n':
+        novo += letra.upper()
+    else:
+        novo += letra
+
+print(novo)
