40e96a521df1edc32874aa3fabdf543813f58f56
lucasfernande <lucas.fernandes6192@outlook.com>
2020-06-18 13:39:20 -0300

    deletando elementos da lista

--- listas/listas.py
+++ listas/listas.py
@@ -13,3 +13,10 @@ l2.insert(0, 'banana') # inserindo um valor dando uma posição a ele
 
 l2.append('b') # inserindo um novo valor na lista
 print(l2)
+
+l2.pop() # remove o ultimo valor da lista
+print(l2)
+
+l3 = [1, 2, 3, 4, 5, 6]
+del(l3[3:5]) # removendo elementos da lista pelo indice
+print(l3)
